1a8084153b9a25f9830ffdb178d8b72d62b57b34
Gevindu2004 <gevindujayarathne@gmail.com>
2025-06-25 11:21:57 +0530

    Started to build the website

--- src/main/java/com/viacodos/viacodos2/HomeController.java
+++ src/main/java/com/viacodos/viacodos2/HomeController.java
@@ -2,11 +2,6 @@ package com.viacodos.viacodos2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.http.ResponseEntity;
-import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -14,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Home";
+        return "index";
     }
 
     @GetMapping("/dashboard")
